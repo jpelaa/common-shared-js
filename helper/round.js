245324ec02d2a1bdd259c2ede6b0341f23681f6a
jpelaa <jpelaa1991994@gmail.com>
2020-02-18 18:39:13 +0530

    Update round.js

--- helper/round.js
+++ helper/round.js
@@ -1,4 +1,8 @@
-
+/**
+ * @param {number} value
+ * @param {number} value
+ * @returns {number} 
+ */
 function round(value, decimals) {
   return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
 }
